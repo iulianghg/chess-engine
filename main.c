2a0b589181bacf9a83ae41deda1f8cab4e994eb4
Julian Gherghel <74251822+iulianghg@users.noreply.github.com>
2021-02-24 18:54:33 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "defs.h"
+
+
+int main() {
+
+    AllInit();
+
+    int PieceOne = rand();
+
+    return 0;
+}
+
+// Episode 13 - have not done anything in this episode
+// https://www.youtube.com/watch?v=vF_Td1nABYw&list=PLZ1QII7yudbc-Ky058TEaOstZHVbT-2hg&index=13
+// Bluefever Software on Youtube
+// "Programming A Chess Engine in C"
